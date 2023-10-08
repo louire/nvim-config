fd2872c6f3b7476f9de8ddb034619ca9cdf744a2
Loui Recio <lreciopersonal@gmail.com>
2023-10-08 18:01:56 -0500

    added plugins and more stuff

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
-require("nycrat")
+--DEFAULT VIM
+--
+--Packer plugin manager config stuff
+return require('packer').startup(function(use)
+	--packer can manage itself
+
+
+	use 'wbthomason/packer.nvim'
+	--wakatime
+	use 'wakatime/vim-wakatime'
+	
+end)
+
+
+
+
