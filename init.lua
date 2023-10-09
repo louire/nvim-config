d1644c85458d9bf4e566e06386b3a0dd027601ce
Loui Recio <lreciopersonal@gmail.com>
2023-10-09 12:48:33 -0500

    added cmp

--- init.lua
+++ init.lua
@@ -24,6 +24,14 @@ require('packer').startup(function(use)
 		end
 	}
 
+	--nvim cmp 
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+
 	--wakatime
 	use 'wakatime/vim-wakatime'
 
